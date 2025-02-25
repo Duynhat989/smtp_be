a7664646b7ed5bb3e7533aae91dad93317f9fda8
Duynhat989 <vietduy989kc@gmail.com>
2025-02-24 16:07:57 -0800

    update ms

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { validate } = require('../middlewares/validation.js');
 const auth = require('../middlewares/authMiddleware.js');
 // Đăng ký Giáo viên mới
-router.post('/register',  validate(['name','email','phone','password']), authController.register);
+router.post('/register',  validate(['email','password']), authController.register);
 
 // Đăng nhập
 router.post('/login',  validate(['email', 'password']), authController.login);
